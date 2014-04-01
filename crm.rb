7cb93229fa212529b02ee37a5bcf4eb8d1cbf94e
Taha Jalil <taha.jalil@gmail.com>
2014-04-01 18:01:00 -0400

    added contacts.erb, edited index.erb

--- crm.rb
+++ crm.rb
@@ -5,3 +5,27 @@ get '/' do
   @create_time = Time.new.strftime("%m-%d-%Y at %H:%M:%S")
   erb :index
 end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  #do something
+end
+
+get '/contacts/:id' do
+  #do something
+end
+
+get '/contacts/:id/edit' do
+  #do something
+end
+
+get '/contacts/:id/remove' do
+  #do something
+end
+
+get '/contacts/:id/notes' do
+  #do something
+end
