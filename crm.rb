1014156e1fc535d97817e430815b7e10ead0dee5
Taha Jalil <taha.jalil@gmail.com>
2014-04-05 16:53:30 -0400

    required data_mapper, removed contact.rb

--- crm.rb
+++ crm.rb
@@ -1,7 +1,22 @@
 require_relative 'rolodex'
-require_relative 'contact'
 require 'sinatra'
 require 'sinatra/content_for'
+require'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+
+  attr_accessor :id, :first_name, :last_name, :email, :notes, :time_created
+
+  def initialize(first_name, last_name, email, notes, time_created)
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
+    @email = email
+    @notes = notes.capitalize
+    @time_created = time_created
+  end
+end
 
 @@rolodex = Rolodex.new
 
